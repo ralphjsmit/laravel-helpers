e4d58d06cf419f2bed9ed82df8467b0ec99532b1
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-01-19 16:26:58 +0100

    Register Livewire for testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace RalphJSmit\Helpers\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RalphJSmit\Helpers\HelpersServiceProvider;
 
@@ -13,7 +14,7 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'RalphJSmit\\Helpers\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn (string $modelName) => 'RalphJSmit\\Helpers\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
         );
     }
 
@@ -21,13 +22,13 @@ class TestCase extends Orchestra
     {
         return [
             HelpersServiceProvider::class,
+            LivewireServiceProvider::class,
         ];
     }
 
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-
         /*
         $migration = include __DIR__.'/../database/migrations/create_laravel-helpers_table.php.stub';
         $migration->up();

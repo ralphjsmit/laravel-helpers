115acb88e4c221bb3de8d4403565e4523a29467e
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-02-02 10:53:36 +0100

    Add carbonDate() helper function

--- src/Laravel/Support/helpers.php
+++ src/Laravel/Support/helpers.php
@@ -6,8 +6,17 @@
 use Illuminate\Support\Carbon;
 
 if ( ! function_exists('carbon') ) {
-    function carbon(mixed $input = null)
+    function carbon(mixed $input = null): Carbon
     {
         return Carbon::parse($input);
+    }
+}
+
+if ( ! function_exists('carbonDate') ) {
+    // This function returns a Carbon instance and floors the date, so
+    // that the time is always at midnight.
+    function carbonDate(mixed $input = null): Carbon
+    {
+        return Carbon::parse($input)->floorDay();
     }
 }
--- tests/Unit/Laravel/Support/HelpersTest.php
+++ tests/Unit/Laravel/Support/HelpersTest.php
@@ -6,8 +6,18 @@ it('can return a carbon::parse instance from carbon()', function () {
     $carbon = carbon();
     expect($carbon)->toBeInstanceOf(Carbon::class);
 
-    $carbon = carbon('2022-01-01 00:00:00');
+    $carbon = carbon('2022-01-31 10:08:00');
     expect($carbon)
         ->toDateTimeString()
-        ->toBe('2022-01-01 00:00:00');
+        ->toBe('2022-01-31 10:08:00');
+});
+
+it('can return a carbon instance for a date', function () {
+    $date = carbonDate();
+    expect($date)->toBeInstanceOf(Carbon::class);
+
+    $date = carbonDate('2022-01-31');
+    expect($date)
+        ->toDateTimeString()
+        ->toBe('2022-01-31 00:00:00');
 });

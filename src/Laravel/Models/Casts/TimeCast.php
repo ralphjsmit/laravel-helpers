0534f1e9ab8ade069a8bfe573e92ea76d86969e1
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-11-21 14:52:52 +0100

    Update TimeCast.php

--- src/Laravel/Models/Casts/TimeCast.php
+++ src/Laravel/Models/Casts/TimeCast.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Carbon;
 
 class TimeCast implements CastsAttributes
 {
-    /** This method is used to convert a raw value from the database into a Carbon instance. */
     public function get($model, string $key, $value, array $attributes): ?Carbon
     {
         if ( $value === null ) {
@@ -17,7 +16,6 @@ class TimeCast implements CastsAttributes
         return carbon($value);
     }
 
-    /** Convert an input like a Carbon instance or a string or null to a raw value for the database. */
     public function set($model, string $key, $value, array $attributes): ?string
     {
         return $value instanceof Carbon
